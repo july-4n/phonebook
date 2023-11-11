dff60bb6a9f5cb34715bfb760813645192ced664
July-4n <september4n@gmail.com>
2023-11-11 23:16:36 +0500

    solve mod2 task5

--- phonebook/js/script.js
+++ phonebook/js/script.js
@@ -23,6 +23,11 @@ const data = [
   },
 ];
 
+const svgEdit = `
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M19.001 3.17163C18.7609 3.17163 18.5231 3.21892 18.3013 3.31081C18.0795 3.4027 17.8779 3.53738 17.7081 3.70716L4.39589 17.0194L3.42622 20.5748L6.98167 19.6052L20.2939 6.29295C20.4637 6.12317 20.5984 5.9216 20.6902 5.69977C20.7821 5.47793 20.8294 5.24017 20.8294 5.00006C20.8294 4.75994 20.7821 4.52218 20.6902 4.30035C20.5984 4.07852 20.4637 3.87695 20.2939 3.70716C20.1241 3.53738 19.9225 3.4027 19.7007 3.31081C19.4789 3.21892 19.2411 3.17163 19.001 3.17163ZM17.5359 1.46305C18.0004 1.27066 18.4982 1.17163 19.001 1.17163C19.5038 1.17163 20.0016 1.27066 20.4661 1.46305C20.9306 1.65545 21.3526 1.93745 21.7081 2.29295C22.0636 2.64845 22.3456 3.07049 22.538 3.53498C22.7304 3.99947 22.8294 4.4973 22.8294 5.00006C22.8294 5.50281 22.7304 6.00064 22.538 6.46513C22.3456 6.92962 22.0636 7.35166 21.7081 7.70716L8.20811 21.2072C8.08505 21.3302 7.93201 21.419 7.76412 21.4648L2.26412 22.9648C1.9179 23.0592 1.54764 22.9609 1.29389 22.7072C1.04014 22.4534 0.941814 22.0832 1.03624 21.7369L2.53624 16.2369C2.58203 16.069 2.67084 15.916 2.79389 15.793L16.2939 2.29295C16.6494 1.93745 17.0714 1.65545 17.5359 1.46305Z" fill="#212529"/>
+  </svg>
+`;
+
 {
   const createContainer = () => {
     const container = document.createElement('div');
@@ -156,7 +161,6 @@ const data = [
     return footer;
   };
 
-
   const renderPhoneBook = (app, title) => {
     const header = createHeader();
     const logo = createLogo(title);
@@ -183,6 +187,9 @@ const data = [
 
     return {
       list: table.tbody,
+      logo,
+      btnAdd: buttonGroup.btns[0],
+      formOverlay: form.overlay,
     };
   };
 
@@ -205,24 +212,65 @@ const data = [
     const phoneLink = document.createElement('a');
     phoneLink.href = `tel:${phone}`;
     phoneLink.textContent = phone;
-
+    tr.phoneLink = phoneLink;
     tdPhone.append(phoneLink);
-    tr.append(tdDel, tdName, tdSurname, tdPhone);
+
+    const tdEdit = document.createElement('td');
+    const buttonEdit = document.createElement('button');
+    buttonEdit.classList.add('btn');
+    buttonEdit.type = 'button';
+    buttonEdit.innerHTML = svgEdit;
+    tdEdit.append(buttonEdit);
+
+    tr.append(tdDel, tdName, tdSurname, tdPhone, tdEdit);
     return tr;
   };
 
   const renderContacts = (elem, data) => {
     const allRow = data.map(createRow);
     elem.append(...allRow);
+    return allRow;
+  };
+
+  const hoverRow = (allRow, logo) => {
+    const text = logo.textContent;
+    allRow.forEach(contact => {
+      contact.addEventListener('mouseenter', () => {
+        logo.textContent = contact.phoneLink.textContent;
+      });
+      contact.addEventListener('mouseleave', () => {
+        logo.textContent = text;
+      });
+    });
   };
 
   const init = (selectorApp, title) => {
     const app = document.querySelector(selectorApp);
     const phoneBook = renderPhoneBook(app, title);
 
-    const {list} = phoneBook;
+    const {list, logo, btnAdd, formOverlay} = phoneBook;
 
-    renderContacts(list, data);
+    const allRow = renderContacts(list, data);
+
+    hoverRow(allRow, logo);
+
+    const objEvent = {
+      handleEvent(event) {
+        if (event.ctrlKey) {
+          this.bar();
+        } else {
+          this.foo();
+        }
+      },
+      bar() {
+        document.body.style.backgroundColor = 'black';
+      },
+      foo() {
+        formOverlay.classList.add('is-visible');
+      },
+    };
+
+    btnAdd.addEventListener('click', objEvent);
   };
 
   window.phoneBookInit = init;
